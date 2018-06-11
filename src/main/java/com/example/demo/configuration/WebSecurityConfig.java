4344cc464528c65fbaf59410c6690c617f8c7ddd
Kazuya HATTORI <k_hattori@ap-com.co.jp>
2018-06-11 18:31:29 +0900

    Use spring-oauth2-serializable

--- src/main/java/com/example/demo/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/demo/configuration/WebSecurityConfig.java
@@ -1,11 +1,12 @@
 package com.example.demo.configuration;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableOAuth2Sso
+import jp.co.ap_com.spring.oauth2.EnableOAuth2Serializable;
+
+@EnableOAuth2Serializable
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
